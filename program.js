1896a9bceaf5dd420adb3f2e48dcaee2e8f661eb
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-01 21:35:26 -0600

    Done with YOu

--- program.js
+++ program.js
@@ -12,6 +12,8 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const mymodule = require('./mymodule.js');
+const url = require('url');
+
 
 
 // <--My first I/0-->
@@ -82,53 +84,106 @@ const mymodule = require('./mymodule.js');
 
 
 //<---------------JUGGLING ASYNC---------->>>>>
+//
+// let url1 = process.argv[2];
+// let url2 = process.argv[3];
+// let url3 = process.argv[4];
+// let url1string = "";
+// let url2string = "";
+// let url3string = "";
+//
+// let url2function = function(){
+//   http.get(url2 , function(response){
+//     response.setEncoding('utf8');
+//     let string = '';
+//     response.on('data', function(word){
+//       string += word;
+//     });
+//     response.on('end', function(){
+//       url2string = string;
+//       console.log(url2string);
+//       url3function();
+//     })
+//   });
+// }
+//
+// let url3function = function(){
+//   http.get(url3 , function(response){
+//     response.setEncoding('utf8');
+//     let string = '';
+//     response.on('data', function(word){
+//       string += word;
+//     });
+//     response.on('end', function(){
+//       url3string = string;
+//       console.log(url3string)
+//     })
+//   });
+// }
+//
+//
+// http.get(url1 , function(response){
+//   response.setEncoding('utf8');
+//   let string = '';
+//   response.on('data', function(word){
+//     string += word;
+//   });
+//   response.on('end', function(){
+//     url1string = string;
+//     console.log(url1string);
+//     url2function();
+//   })
+// });
 
-let url1 = process.argv[2];
-let url2 = process.argv[3];
-let url3 = process.argv[4];
-let url1string = "";
-let url2string = "";
-let url3string = "";
+// //<----------TIME SERVER--------->
+// const net = require('net');
+// const strftime = require('strftime');
+//
+// var server = net.createServer(function (socket) {
+//   socket.write(strftime('%Y-%m-%d %H:%M'));
+//   socket.end('\n');
+// })
+// server.listen(process.argv[2]);
+//<--------HTTP FILE SEVER-------->
+// const port = process.argv[2];
+// var files = process.argv[3];
+// var server = http.createServer((req, res)=>{
+//   fs.createReadStream(files).pipe(res);
+// });
+//
+// server.listen(port);
 
-let url2function = function(){
-  http.get(url2 , function(response){
-    response.setEncoding('utf8');
-    let string = '';
-    response.on('data', function(word){
-      string += word;
-    });
-    response.on('end', function(){
-      url2string = string;
-      console.log(url2string);
-      url3function();
-    })
-  });
-}
+//<---------HTTP UPPERCASERER------>
 
-let url3function = function(){
-  http.get(url3 , function(response){
-    response.setEncoding('utf8');
-    let string = '';
-    response.on('data', function(word){
-      string += word;
-    });
-    response.on('end', function(){
-      url3string = string;
-      console.log(url3string)
-    })
-  });
-}
+// var map = require('through2-map')
+// var server = http.createServer(function(req, res){
+//   req.pipe(map(function (chunk) {
+//    return chunk.toString().toUpperCase();
+//  })).pipe(res);
+// }).listen(process.argv[2]);
 
+//<--------HTTP JSON API SERVER----->
 
-http.get(url1 , function(response){
-  response.setEncoding('utf8');
-  let string = '';
-  response.on('data', function(word){
-    string += word;
-  });
-  response.on('end', function(){
-    url1string = string;
-    console.log(url1string);
-    url2function();
-  })
-});
+const port = process.argv[2];
+var server = http.createServer(function(req, res){
+  var pathUrl = url.parse(req.url, true);
+  var resultObj ={};
+  if(req.method !== 'GET'){
+    return res.write('ERRRRROR')
+    res.end();
+  }else if(pathUrl.pathname === '/api/parsetime'){
+    var newDate = new Date(pathUrl.query.iso);
+    resultObj.hour = newDate.getHours();
+    resultObj.minute = newDate.getMinutes();
+    resultObj.second = newDate.getSeconds();
+    res.write(JSON.stringify(resultObj));
+    res.end();
+  }else if(pathUrl.pathname === '/api/unixtime'){
+    resultObj.unixtime = Date.parse(pathUrl.query.iso);
+    res.write(JSON.stringify(resultObj));
+    res.end();
+  }else {
+    res.sendstatus(404);
+    res.end();
+  }
+}).listen(port);
